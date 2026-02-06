8f31ad220f2ed02bdd33fe21d7cf6051e6a9b114
timriffe <tim.riffe@gmail.com>
2026-02-06 10:46:35 +0100

    .

--- tests/testthat/test-symmetry.R
+++ tests/testthat/test-symmetry.R
@@ -10,17 +10,29 @@ test_that("Symmetric methods reverse sign when mx1 and mx2 are swapped", {
   sym_methods <- c(
     "arriaga_sym",
     "lopez_ruzicka_sym",
+    "andreev_sym",
+    "sen_arriaga_sym",
+    "sen_lopez_ruzicka_sym",
+    "sen_andreev_sym",
     "chandrasekaran_ii",
     "chandrasekaran_iii",
     "stepwise",
-    "horiuchi"
+    "horiuchi",
+    "numerical",
+    "lifetable",
+    "sen_arriaga_sym_inst",
+    "sen_andreev_sym_inst",
+    "sen_lopez_ruzicka_sym_inst",
+    "sen_arriaga_sym_inst2",
+    "sen_andreev_sym_inst2",
+    "sen_lopez_ruzicka_sym_inst2"
   )
 
   for (m in sym_methods) {
-    d1 <- LEdecomp(mx1, mx2, age = x, method = m)
-    d2 <- LEdecomp(mx2, mx1, age = x, method = m)
+    d1 <- LEdecomp(mx1, mx2, age = x, method = m, tol=1e-14, perturb = 1e-6)
+    d2 <- LEdecomp(mx2, mx1, age = x, method = m, tol=1e-14, perturb = 1e-6)
 
-    expect_equal(d1$LEdecomp, -d2$LEdecomp, tolerance = 1e-10,
+    expect_equal(d1$LEdecomp, -d2$LEdecomp, tolerance = 1e-7,
                  info = paste("Symmetry test failed for method:", m))
   }
 })
